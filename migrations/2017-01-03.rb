8e99bf348848559d40d2e1517c1fda3d44599824
Scott Dubinsky <smdubinsky@gmail.com>
2017-03-20 22:42:24 +0200

    update db creation script and blog posting script

--- migrations/2017-01-03.rb
+++ migrations/2017-01-03.rb
@@ -1,5 +1,6 @@
 "CREATE TABLE blog (
 post TEXT,
 id SERIAL CONSTRAINT blog_pk PRIMARY KEY,
-title VARCHAR(200)
+title VARCHAR(200),
+tags TEXT,
 uploaded_date DATE);"
---
+++ post_blog.rb
@@ -0,0 +1,15 @@
+require 'pg'
+require 'time'
+
+#Usage:
+#DATABASE_URL=$(heroku config:get DATABASE_URL -a sdubinsky) ruby post_blog.rb $FILENAME [$tag1 $tag2...]
+$db_url = ENV['DATABASE_URL']
+connection = PG.connect($db_url)
+
+filename = ARGV[0]
+$tags = ARGV[1..-1]
+$title = File.basename(filename, ".*")
+$post = File.read(filename)
+$now = Time.now
+command = "INSERT INTO blog (title, post, tags, uploaded_date) VALUES ('#{$title}', '#{$post.gsub("'", "''")}', '#{$tags.join(", ")}', '#{$now.strftime('%Y-%m-%d')}')"
+connection.exec(command)
